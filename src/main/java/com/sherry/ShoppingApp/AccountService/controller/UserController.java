89d81459a3ae0aa696c16845853690f0619ed200
Sherry <iamsherrysingh@gmail.com>
2020-04-25 12:30:03 -0400

    Added sql script to reconstruct database

--- src/main/java/com/sherry/ShoppingApp/AccountService/controller/UserController.java
+++ src/main/java/com/sherry/ShoppingApp/AccountService/controller/UserController.java
@@ -6,10 +6,12 @@ import com.sherry.ShoppingApp.AccountService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping
 public class UserController {
 
     @Autowired
@@ -18,7 +20,7 @@ public class UserController {
     @Autowired
     UserDAO userDAO;
 
-    @GetMapping("/list")
+    @GetMapping("/users")
     public List<User> listUsers() {
         List<User> users= userDAO.findAll();
         users.forEach(System.out::println);
