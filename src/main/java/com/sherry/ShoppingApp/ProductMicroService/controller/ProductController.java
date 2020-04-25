9113be9ce8341391054f80c99fa4d5ead6f5d3d0
Sherry <iamsherrysingh@gmail.com>
2020-04-25 19:45:44 -0400

    Added productimage.path variable to ProductMicroService application.properties

--- src/main/java/com/sherry/ShoppingApp/ProductMicroService/controller/ProductController.java
+++ src/main/java/com/sherry/ShoppingApp/ProductMicroService/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.sherry.ShoppingApp.ProductMicroService.model.Product;
 import com.sherry.ShoppingApp.ProductMicroService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -36,5 +35,4 @@ public class ProductController {
     public void addProduct(@RequestBody Product product){
         productService.addProduct(product);
     }
-
 }
