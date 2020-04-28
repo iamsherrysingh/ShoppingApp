ebc36237d6ccbb2a4cd88d04f51b745a6746ff1f
Sherry <iamsherrysingh@gmail.com>
2020-04-27 22:50:13 -0400

    Updated Product MicroService sql script

--- src/main/java/com/sherry/ProductMicroService/shopping_app_product_db.sql
+++ src/main/java/com/sherry/ProductMicroService/shopping_app_product_db.sql
@@ -1,19 +1,19 @@
-DROP DATABASE IF EXISTS shopping_app_product_db;
-CREATE database shopping_app_product_db;
-USE shopping_app_product_db;
-DROP TABLE if exists product;
-SHOW TABLES;
-CREATE TABLE product (
-                         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         product_name VARCHAR(50) NOT NULL,
-                         product_description VARCHAR(300) NOT NULL,
-                         product_image VARCHAR(150),
-                         price double NOT null
-);
-SHOW TABLES;
-INSERT INTO product VALUES(1, 'Baseball Bat', 'Full size aluminum baseball bat', 'IMG', 79.99);
-INSERT INTO product VALUES(2, 'Network Switch', 'Gigabit Network Switch', 'IMG', 19.99);
-INSERT INTO product VALUES(3, 'CAT-5 Ethernet Cable', 'Gigabit Network Cable, 50 feet', 'IMG', 14.99);
-INSERT INTO product VALUES(4, 'Google Pixel 4XL', 'Snapdragon 855, 6GB RAM, 646GB', 'IMG', 1299.99);
-INSERT INTO product VALUES(5, 'Mug', '250 ml Coffee Mug', 'IMG', 3.99);
-SELECT * FROM product;
+	DROP DATABASE IF EXISTS shopping_app_product_db;
+	CREATE database shopping_app_product_db;
+	USE shopping_app_product_db;
+	DROP TABLE if exists product;
+	SHOW TABLES;
+	CREATE TABLE product (
+							   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+								product_name VARCHAR(50) NOT NULL,
+								product_description VARCHAR(300) NOT NULL,
+								product_image VARCHAR(150),
+								price double NOT null
+							 );
+	SHOW TABLES;
+	INSERT INTO product VALUES(1, 'Baseball Bat', 'Full size aluminum baseball bat', 'product_1.jpg', 79.99);
+	INSERT INTO product VALUES(2, 'Network Switch', 'Gigabit Network Switch', 'product_2.jpg', 19.99);
+	INSERT INTO product VALUES(3, 'CAT-5 Ethernet Cable', 'Gigabit Network Cable, 50 feet', 'product_3.jpg', 14.99);
+	INSERT INTO product VALUES(4, 'Google Pixel 4XL', 'Snapdragon 855, 6GB RAM, 646GB', 'product_4.jpg', 1299.99);
+	INSERT INTO product VALUES(5, 'Mug', '250 ml Coffee Mug', 'product_5.jpg', 3.99);
+	SELECT * FROM product;
