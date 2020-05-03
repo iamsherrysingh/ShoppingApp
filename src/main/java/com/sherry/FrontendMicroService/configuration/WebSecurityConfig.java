d48f25b612458358597a704142e7b35126dbb382
Sherry <iamsherrysingh@gmail.com>
2020-05-02 20:06:22 -0400

    Enabled authorization. Basic authentication does not work anymore.
    Signed-off-by: Sherry <iamsherrysingh@gmail.com>

--- src/main/java/com/sherry/FrontendMicroService/configuration/WebSecurityConfig.java
+++ src/main/java/com/sherry/FrontendMicroService/configuration/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sherry.FrontendMicroService.configuration;
 
+import com.sherry.FrontendMicroService.filters.JwtRequestFilter;
 import com.sherry.FrontendMicroService.service.MyUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +10,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -19,17 +22,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     MyUserDetailService myUserDetailService;
 
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
+        /*http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/user").hasAnyAuthority("USER", "ADMIN")
                 .anyRequest().permitAll()  //Permitting access to remaining endpoints
-                .and().httpBasic();
+                .and().httpBasic();*/
+
+
+        //Enabled authorization. Basic authentication does not work anymore
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/user").hasAnyAuthority("USER", "ADMIN")
+                .anyRequest().permitAll()//Permitting access to remaining endpoints
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
---
+++ src/main/java/com/sherry/FrontendMicroService/filters/JwtRequestFilter.java
@@ -0,0 +1,55 @@
+package com.sherry.FrontendMicroService.filters;
+
+import com.sherry.FrontendMicroService.service.MyUserDetailService;
+import com.sherry.FrontendMicroService.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    MyUserDetailService myUserDetailService;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        String jwt = null;
+        String username = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.myUserDetailService.loadUserByUsername(username);
+            System.out.println("=====" + userDetails.getUsername() + userDetails.getPassword() + userDetails.getAuthorities());
+
+
+            System.out.println("*****" + userDetails);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
