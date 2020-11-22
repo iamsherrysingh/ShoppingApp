ac14160738c3aa4abcc4b7d269c53e7cf930e7ce
Sehaj Singh <sehaj.singh@tecsys.com>
2020-11-22 15:08:01 -0500

    passwordValidation microservice added

--- accounts-microservice/src/main/java/com/sherry/accounts/controller/AccountsController.java
+++ accounts-microservice/src/main/java/com/sherry/accounts/controller/AccountsController.java
@@ -1,12 +1,21 @@
 package com.sherry.accounts.controller;
 
-import com.sherry.accounts.model.Customer;
-import com.sherry.accounts.service.customerService;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.sherry.accounts.model.Customer;
+import com.sherry.accounts.service.customerService;
 
 @RestController
 @RequestMapping("/accounts")
@@ -37,12 +46,30 @@ public class AccountsController {
     }
 
     @PostMapping("/customer")
-    public void addCustomer(@RequestBody Customer customer) {
-        customerService.addCustomer(customer);
+    public boolean addCustomer(@RequestBody Customer customer) {
+    	if(validatePassword(customer)) {
+    		customerService.addCustomer(customer);
+    		return true;
+    	}
+    	return false;
     }
 
     @DeleteMapping("/customer/{customerName}")
     public void deleteCustomer(@PathVariable String customerName) {
         customerService.deleteCustomer(customerName);
+    }
+    
+    @PostMapping("/validatePassword")
+    public boolean validatePassword(@RequestBody Customer customer) {
+    	String patternString= "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$";
+    	Pattern pattern= Pattern.compile(patternString);
+    	Matcher matcher= pattern.matcher(customer.getPassword());
+
+		if(customer.getPassword().length() < 8
+				|| customer.getPassword().contains(customer.getCustomerName()) ) 
+			return false;  
+		if(matcher.matches() == false) //Provided password does not match the regex requirements
+			return false;
+		return true;
     }
 }
