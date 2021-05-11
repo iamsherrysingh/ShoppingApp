2724827af61731bd6ec4d5d6999eb69eabf513ed
Sehaj <iamsherrysingh@gmail.com>
2021-05-10 23:53:57 -0400

    Added proper logging

--- frauddetection-microservice/src/main/java/com/sherry/frauddetection/FraudDetectionApplication.java
+++ frauddetection-microservice/src/main/java/com/sherry/frauddetection/FraudDetectionApplication.java
@@ -8,6 +8,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+//import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,9 +16,12 @@ import com.sherry.frauddetection.model.Order;
 
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @SpringBootApplication
 public class FraudDetectionApplication {
+//	private static Logger LOG = LoggerFactory.getLogger(FraudDetectionApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FraudDetectionApplication.class, args);
@@ -32,15 +36,13 @@ public class FraudDetectionApplication {
 		StreamsBuilder streamsBuilder = new StreamsBuilder();
 
 		KStream<String, Order> stream = streamsBuilder.stream("payments");
-		stream
-//		.peek((transactionId, order) -> System.out.println(order))
+		stream.peek(FraudDetectionApplication::printOnEnter)
 //				.filter((transactionId, order) -> !order.getUserId().toString().equals(""))
 //				.filter((transactionId, order) -> order.getNbOfItems() < 1000)
 //				.filter((transactionId, order) -> order.getTotalAmount() <= 10000).mapValues((order) -> {
 //					order.setUserId(String.valueOf(order.getUserId()).toUpperCase());
 //					return order;})
-//				.peek((transactionId, order) -> System.out.println(order))
-				.to("validated-payments");
+				.peek(FraudDetectionApplication::printOnExit).to("validated-payments");
 
 		Topology topology = streamsBuilder.build();
 
@@ -49,6 +51,18 @@ public class FraudDetectionApplication {
 		streams.start();
 
 		Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+	}
+
+	private static void printOnEnter(String transactionId, Order order) {
+		log.info("*******************");
+		log.info("Entering stream with Transaction ID" + transactionId);
+		log.info("and order: " + order);
+	}
+
+	private static void printOnExit(String transactionId, Order order) {
+		log.info("*******************");
+		log.info("Exiting stream with Transaction ID" + transactionId);
+		log.info("and order: " + order);
 	}
 
 }
