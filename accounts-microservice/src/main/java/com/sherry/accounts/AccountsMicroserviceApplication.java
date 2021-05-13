5cc616527f2c7044529746f5ad6a588ee7a0ae86
DIR\sehajpreet.s.gill <sehajpreet.s.gill@accenture.com>
2021-05-13 17:23:02 -0400

    trying something

--- accounts-microservice/src/main/java/com/sherry/accounts/AccountsMicroserviceApplication.java
+++ accounts-microservice/src/main/java/com/sherry/accounts/AccountsMicroserviceApplication.java
@@ -20,39 +20,38 @@ public class AccountsMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsMicroserviceApplication.class, args);
 
-		// Consumer 1 config
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("group.id", "user-tracking-consumer");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-		consumer.subscribe(Arrays.asList("users"));
-
-		// Consumer 2 config
-		Properties props2 = new Properties();
-		props2.put("bootstrap.servers", "localhost:9092");
-		props2.put("group.id", "user-tracking-avro-group");
-		props2.put("key.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
-		props2.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
-		props2.put("specific.avro.reader", "true");
-		props2.put("schema.registry.url", "http://localhost:8081");
-
-		KafkaConsumer<User, Product> consumer2 = new KafkaConsumer<>(props2);
-		consumer2.subscribe(Arrays.asList("user-tracking-avro"));
-
-		// Consume Events
-		while (true) {
-			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-			ConsumerRecords<User, Product> records2 = consumer2.poll(Duration.ofMillis(100));
-
-			for (ConsumerRecord<String, String> record : records) {
-				System.out.println("Kafka Consumer ==>" + record.key() + " " + record.value());
-			}
-			for (ConsumerRecord<User, Product> record : records2) {
-				System.out.println("Kafka Consumer ==>" + record.key() + " " + record.value());
-			}
-		}
+		/*
+		 * // Consumer 1 config Properties props = new Properties();
+		 * props.put("bootstrap.servers", "localhost:9092"); props.put("group.id",
+		 * "user-tracking-consumer"); props.put("key.deserializer",
+		 * "org.apache.kafka.common.serialization.StringDeserializer");
+		 * props.put("value.deserializer",
+		 * "org.apache.kafka.common.serialization.StringDeserializer");
+		 * 
+		 * KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		 * consumer.subscribe(Arrays.asList("users"));
+		 * 
+		 * // Consumer 2 config Properties props2 = new Properties();
+		 * props2.put("bootstrap.servers", "localhost:9092"); props2.put("group.id",
+		 * "user-tracking-avro-group"); props2.put("key.deserializer",
+		 * "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+		 * props2.put("value.deserializer",
+		 * "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+		 * props2.put("specific.avro.reader", "true"); props2.put("schema.registry.url",
+		 * "http://localhost:8081");
+		 * 
+		 * KafkaConsumer<User, Product> consumer2 = new KafkaConsumer<>(props2);
+		 * consumer2.subscribe(Arrays.asList("user-tracking-avro"));
+		 * 
+		 * // Consume Events while (true) { ConsumerRecords<String, String> records =
+		 * consumer.poll(Duration.ofMillis(100)); ConsumerRecords<User, Product>
+		 * records2 = consumer2.poll(Duration.ofMillis(100));
+		 * 
+		 * for (ConsumerRecord<String, String> record : records) {
+		 * System.out.println("Kafka Consumer ==>" + record.key() + " " +
+		 * record.value()); } for (ConsumerRecord<User, Product> record : records2) {
+		 * System.out.println("Kafka Consumer ==>" + record.key() + " " +
+		 * record.value()); } }
+		 */
 	}
 }
